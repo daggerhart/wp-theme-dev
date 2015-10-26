9868e45d2d45f22a3ce918543020537d3d5ec854
Jonathan Daggerhart <jonathan@daggerhart.com>
2015-10-26 02:19:31 -0400

    lightbox-ready fancy-large thumbnails

--- advanced/partials/entry-thumbnail-fancy.php
+++ advanced/partials/entry-thumbnail-fancy.php
@@ -4,9 +4,10 @@ if ( has_post_thumbnail() )
 	// use a special version of the featured image as a fancy header image
 	$thumb_id = get_post_thumbnail_id( get_the_ID() );
 	$thumb = wp_get_attachment_image_src( $thumb_id, 'fancy-large' );
+	$full = wp_get_attachment_image_src( $thumb_id, 'full' );
 	?>
 	<div class="fancy-large-wrapper" style="background-image: url(<?php echo $thumb[0]; ?>);">
-		<div class="fancy-large-overlay"></div>
+		<a class="fancy-large-overlay" href="<?php echo esc_url( $full[0] ); ?>" rel="lightbox"></a>
 	</div>
 	<?php
 }
